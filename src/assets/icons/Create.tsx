57db9a24f08fa1cb4bad232eddc8d290e00fe90c
xaosland <xaosland@mail.ru>
2024-07-12 21:04:38 +0400

    feat: add Icons for sidebar component

---
+++ src/assets/icons/Create.tsx
@@ -0,0 +1,34 @@
+import { type Ref, type SVGProps, forwardRef, memo } from 'react'
+const CreateIcon = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
+  <svg
+    fill={'none'}
+    height={'1em'}
+    ref={ref}
+    viewBox={'0 0 24 24'}
+    width={'1em'}
+    xmlns={'http://www.w3.org/2000/svg'}
+    {...props}
+  >
+    <g clipPath={'url(#a)'}>
+      <path
+        d={
+          'M18 3H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3Zm1 15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v12Z'
+        }
+        fill={'#fff'}
+      />
+      <path
+        d={'M15 11h-2V9a1 1 0 0 0-2 0v2H9a1 1 0 0 0 0 2h2v2a1 1 0 0 0 2 0v-2h2a1 1 0 0 0 0-2Z'}
+        fill={'#fff'}
+      />
+    </g>
+    <defs>
+      <clipPath id={'a'}>
+        <path d={'M0 0h24v24H0z'} fill={'#fff'} />
+      </clipPath>
+    </defs>
+  </svg>
+)
+const ForwardRef = forwardRef(CreateIcon)
+const Memo = memo(ForwardRef)
+
+export default Memo
---
+++ src/assets/icons/Messenger.tsx
@@ -0,0 +1,36 @@
+import { type Ref, type SVGProps, forwardRef, memo } from 'react'
+const MessengerIcon = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
+  <svg
+    fill={'none'}
+    height={'1em'}
+    ref={ref}
+    viewBox={'0 0 24 24'}
+    width={'1em'}
+    xmlns={'http://www.w3.org/2000/svg'}
+    {...props}
+  >
+    <g clipPath={'url(#a)'}>
+      <path
+        d={
+          'M12 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm4 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm-8 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z'
+        }
+        fill={'#fff'}
+      />
+      <path
+        d={
+          'M19.07 4.93a10 10 0 0 0-16.28 11c.096.199.127.422.09.64L2 20.8a1 1 0 0 0 .605 1.13A1 1 0 0 0 3 22h.2l4.28-.86a1.26 1.26 0 0 1 .64.09 10 10 0 0 0 11-16.28l-.05-.02Zm.83 8.36a7.999 7.999 0 0 1-11 6.08 3.261 3.261 0 0 0-1.25-.26 3.435 3.435 0 0 0-.56.05l-2.82.57.57-2.82a3.09 3.09 0 0 0-.21-1.81 8 8 0 0 1 9.826-10.72 8 8 0 0 1 5.444 8.91Z'
+        }
+        fill={'#fff'}
+      />
+    </g>
+    <defs>
+      <clipPath id={'a'}>
+        <path d={'M0 0h24v24H0z'} fill={'#fff'} />
+      </clipPath>
+    </defs>
+  </svg>
+)
+const ForwardRef = forwardRef(MessengerIcon)
+const Memo = memo(ForwardRef)
+
+export default Memo
