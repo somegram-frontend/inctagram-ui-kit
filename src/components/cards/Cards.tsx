53620c8dd51f841a21182343dcfd86e2f6189072
xaosland <xaosland@mail.ru>
2024-07-12 09:53:21 +0400

    feat: Cards styles

---
+++ src/components/cards/Cards.tsx
@@ -0,0 +1,21 @@
+import { type ComponentPropsWithoutRef, forwardRef } from 'react'
+
+import clsx from 'clsx'
+
+import s from './cards.module.scss'
+
+type CardProps = {} & ComponentPropsWithoutRef<'div'>
+
+export const Cards = forwardRef<HTMLDivElement, CardProps>(
+  ({ children, className, ...rest }: CardProps, ref) => {
+    const classNames = {
+      card: clsx(s.card, className),
+    }
+
+    return (
+      <div ref={ref} {...rest} className={classNames.card}>
+        {children}
+      </div>
+    )
+  }
+)
---
+++ src/components/cards/cards.stories.tsx
@@ -0,0 +1,23 @@
+import { type Meta, type StoryObj } from '@storybook/react'
+
+import { Cards } from './Cards'
+
+const meta = {
+  component: Cards,
+  tags: ['autodocs'],
+  title: 'Components/Cards',
+} satisfies Meta<typeof Cards>
+
+export default meta
+type Story = StoryObj<typeof Cards>
+export const Default = {
+  args: {
+    children: (
+      <>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus commodi cum delectus
+        dignissimos doloribus facere facilis illo minima molestiae nemo nesciunt nobis quasi,
+        quisquam sapiente totam veniam voluptates? Ducimus, perspiciatis.
+      </>
+    ),
+  },
+} satisfies Story
