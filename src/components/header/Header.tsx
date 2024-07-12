c750609c85579244a7670d4e06c31ce9a2dbe101
xaosland <xaosland@mail.ru>
2024-07-12 13:47:31 +0400

    refactor: add Flag Icon, add headers style

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,32 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Button } from '../button'
+import styles from './header.module.scss'
+
+import { Bell } from '../../assets/icons/Bell'
+import { EnglandFlag } from '../../assets/icons/EnglandFlag'
+import { RussianFlag } from '../../assets/icons/RussianFlag'
 import { Select } from '../select/Select'
 
-export const HeaderWidget = () => {
+type HeaderProps = {
+  isAuth: boolean
+}
+export const Header = ({ isAuth }: HeaderProps) => {
   const options = [
-    { label: 'label1', value: 'value1' },
-    { label: 'label2', value: 'value2' },
+    {
+      label: (
+        <div className={styles.flagContainer}>
+          <EnglandFlag /> &nbsp; <span> English</span>
+        </div>
+      ),
+      value: 'value1',
+    },
+    {
+      label: (
+        <div className={styles.flagContainer}>
+          <RussianFlag />
+          &nbsp;
+          <span>Russian</span>
+        </div>
+      ),
+      value: 'value2',
+    },
   ]
 
-  return (
-    <header>
-      <div>
-        Somegram
-        <div>
-          <Select options={options} />
-          <div>
-            <Button as={'a'}>
-              <a href={'/signin'}>signin</a>
-            </Button>
-            <div>
-              <Button variant={'primary'}>
-                <a href={'/signup'}>signup</a>
-              </Button>
-            </div>
-          </div>
+  return isAuth ? (
+    <header className={styles.header}>
+      <div className={styles.logo}>Somegram</div>
+      <nav className={styles.register}>
+        <div className={styles.bell}>
+          <Bell />
         </div>
-      </div>
+        <Select
+          className={styles.dropdown}
+          options={options}
+          placeholder={
+            <div className={styles.flagContainer}>
+              <EnglandFlag /> &nbsp; <span> English</span>
+            </div>
+          }
+        />
+      </nav>
+    </header>
+  ) : (
+    <header className={styles.header}>
+      <div className={styles.logo}>Somegram</div>
+      <nav className={styles.register}>
+        <Select
+          className={styles.dropdown}
+          options={options}
+          placeholder={
+            <div className={styles.flagContainer}>
+              <EnglandFlag /> &nbsp; <span> English</span>
+            </div>
+          }
+        />
+        <div className={styles.buttons}>
+          <button className={styles.button}>Log in</button>
+          <button className={styles.button}>Sign up</button>
+        </div>
+      </nav>
     </header>
   )
 }
