41c961ca5de1d7cd58c94c33ce028dc89c579f15
ViktorChizh <victor.chizh@mail.ru>
2024-07-13 15:04:51 +0300

    fix: index

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,7 @@
 import { themes } from '@storybook/theming'
 import type { Preview } from '@storybook/react'
 import '../dist/style.css'
-import '../src/styles/index.scss'
+
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/500.css'
 import '@fontsource/inter/600.css'
