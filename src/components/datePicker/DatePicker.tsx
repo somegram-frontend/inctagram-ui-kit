a61085875abdd61dab7013d30bbad2eb559832f8
AnatolyNikol <fanka4ectba@mail.ru>
2024-07-22 20:41:35 +0500

    feature/#14-date-picker-component feat: finish datePicker component

--- src/components/datePicker/DatePicker.tsx
+++ src/components/datePicker/DatePicker.tsx
@@ -4,66 +4,91 @@ import clsx from 'clsx'
 import { format } from 'date-fns'
 import { enGB } from 'date-fns/locale'
 
-// import 'react-datepicker/dist/react-datepicker.css'
-
+import InputStyles from '../input/input.module.scss'
 import s from './datePicker.module.scss'
 
 import SvgCalendar from '../../assets/icons/Calendar'
-import CalendarOutline from '../../assets/icons/CalendarOutline'
 import ChevronLeft from '../../assets/icons/ChevronLeft'
 import ChevronRight from '../../assets/icons/ChevronRight'
 import { Input, type InputProps } from '../input/Input'
 import { Typography } from '../typography/Typography'
 
 type DatePickerProps = {
-  className?: string
-  endDate?: Date
+  disabled?: boolean
+  endDate?: Date | undefined
+  errorMessage?: string
   label?: string
-  setEndDate?: (date: Date | null) => void
-  setStartDate: (date: Date | null) => void
-  startDate: Date
+  selectsRange?: boolean
+  setEndDate?: (date: Date | undefined) => void
+  setStartDate: (date: Date | undefined) => void
+  startDate: Date | undefined
 }
 
 export const DatePickerComponent = (props: DatePickerProps) => {
-  const { className, endDate, label, setEndDate, setStartDate, startDate } = props
-  const isRange = endDate !== undefined
+  const {
+    disabled,
+    endDate,
+    errorMessage,
+    label,
+    selectsRange,
+    setEndDate,
+    setStartDate,
+    startDate,
+  } = props
 
-  const onChangeHandler = (dates: [Date | null, Date | null] | Date) => {
+  const onChangeHandler = (dates: [Date | null, Date | null] | Date | null) => {
+    if (!dates) {
+      return
+    }
+
     if (Array.isArray(dates)) {
+      if (dates.length !== 2) {
+        return
+      }
       const [start, end] = dates
 
-      setStartDate(start)
-      setEndDate?.(end)
+      setStartDate(start || undefined)
+      setEndDate?.(end || undefined)
     } else {
       setStartDate(dates)
     }
   }
 
   return (
-    <div className={clsx(s.box, className)}>
+    <div className={s.box}>
       <DatePicker
         calendarClassName={s.calendar}
-        customInput={<RenderCustomInput />}
+        className={s.datePicker}
+        customInput={
+          <RenderCustomInput disabled={disabled} errorMessage={errorMessage} label={label} />
+        }
         dateFormat={'dd/MM/yyyy'}
         dayClassName={(date: Date) => s.dayDate}
+        disabled={disabled}
         endDate={endDate}
+        errorMessage={errorMessage}
         locale={enGB}
         onChange={onChangeHandler}
+        popperPlacement={'bottom-start'}
         renderCustomHeader={RenderCustomHeader}
         selected={startDate}
-        selectsRange={isRange}
+        selectsRange={selectsRange || undefined}
+        showPopperArrow={false}
         startDate={startDate}
+        toggleCalendarOnIconClick
       />
     </div>
   )
 }
 
-const RenderCustomInput = ({ className, ...rest }: InputProps) => {
+const RenderCustomInput = ({ className, disabled, errorMessage, label, ...rest }: InputProps) => {
   return (
     <Input
-      className={s.input}
-      icon={<SvgCalendar className={clsx(s.calendarIcon)} />}
-      label={'label'}
+      className={clsx(s.dateInput, InputStyles.input, errorMessage && s.hasError)}
+      disabled={disabled}
+      errorMessage={errorMessage}
+      icon={<SvgCalendar className={clsx(s.calendarIcon, errorMessage && s.hasError)} />}
+      label={label}
       {...rest}
     />
   )
--- src/components/datePicker/datePicker.stories.tsx
+++ src/components/datePicker/datePicker.stories.tsx
@@ -15,28 +15,73 @@ type Story = StoryObj<typeof DatePickerComponent>
 
 export const Default = {
   args: {
-    label: 'Label',
+    label: 'Date',
   },
   render: args => {
     const { label } = args
-    const [startDate, setStartDate] = useState<Date | null>(null)
+    const [startDate, setStartDate] = useState<Date | undefined>(undefined)
 
     return <DatePickerComponent label={label} setStartDate={setStartDate} startDate={startDate} />
   },
 } satisfies Story
 
-export const DateRange = {
-  args: {},
+export const Error = {
+  args: {
+    errorMessage: 'Error!',
+    label: 'Date',
+  },
   render: args => {
-    const {} = args
+    const { errorMessage, label } = args
+    const [startDate, setStartDate] = useState<Date | undefined>(undefined)
 
-    const [startDate, setStartDate] = useState<Date | null>(null)
-    const [endDate, setEndDate] = useState<Date | null>(null)
+    return (
+      <DatePickerComponent
+        errorMessage={errorMessage}
+        label={label}
+        setStartDate={setStartDate}
+        startDate={startDate}
+      />
+    )
+  },
+} satisfies Story
+
+export const DateRange = {
+  args: {
+    label: 'Date range',
+    selectsRange: true,
+  },
+  render: args => {
+    const { label, selectsRange } = args
+
+    const [startDate, setStartDate] = useState<Date | undefined>(undefined)
+    const [endDate, setEndDate] = useState<Date | undefined>(undefined)
 
     return (
       <DatePickerComponent
         endDate={endDate}
+        label={label}
+        selectsRange={selectsRange}
         setEndDate={setEndDate}
+        setStartDate={setStartDate}
+        startDate={startDate}
+      />
+    )
+  },
+} satisfies Story
+
+export const Disabled = {
+  args: {
+    disabled: true,
+    label: 'Date',
+  },
+  render: args => {
+    const { disabled, label } = args
+    const [startDate, setStartDate] = useState<Date | undefined>(undefined)
+
+    return (
+      <DatePickerComponent
+        disabled={disabled}
+        label={label}
         setStartDate={setStartDate}
         startDate={startDate}
       />
