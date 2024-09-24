953f919b9355d9dd076d769cbe0b616d6d2b396f
Anatoly Nikolenko <fanka4ectba@mail.ru>
2024-09-24 21:17:26 +0500

    fix: change custom input in Date Picker component

--- src/components/datePicker/DatePicker.tsx
+++ src/components/datePicker/DatePicker.tsx
@@ -11,35 +11,7 @@ import { CalendarOutline, ChevronLeft, ChevronRight } from '../../assets/icons'
 
 import { Input, type InputProps } from '../input/Input'
 import { Typography } from '../typography'
-
-const CustomInput = ({ className, disabled, errorMessage, label, ...rest }: InputProps) => {
-  return (
-    <Input
-      className={clsx(s.dateInput, inputStyles.input, errorMessage && s.hasError)}
-      disabled={disabled}
-      errorMessage={errorMessage}
-      // icon={<CalendarOutline className={clsx(s.calendarIcon, errorMessage && s.hasError)} />}
-      label={label}
-      {...rest}
-    />
-  )
-}
-
-const CustomHeader = ({ date, decreaseMonth, increaseMonth }: ReactDatePickerCustomHeaderProps) => {
-  return (
-    <div className={s.headerContainer}>
-      <div className={s.monthsYear}>
-        <Typography variant={'bold_text16'}>{format(date, 'LLLL y')}</Typography>{' '}
-      </div>
-      <button className={s.button} onClick={decreaseMonth} type={'button'}>
-        {<ChevronLeft />}
-      </button>
-      <button className={s.button} onClick={increaseMonth} type={'button'}>
-        {<ChevronRight />}
-      </button>
-    </div>
-  )
-}
+import { forwardRef, ReactNode } from 'react'
 
 export type DatePickerProps = {
   disabled?: boolean
@@ -104,6 +76,60 @@ export const DatePicker = (props: DatePickerProps) => {
         startDate={startDate}
         toggleCalendarOnIconClick
       />
+    </div>
+  )
+}
+
+// const CustomInput = ({ className, disabled, errorMessage, label, ...rest }: InputProps) => {
+//   return (
+//     <Input
+//       className={clsx(s.dateInput, inputStyles.input, errorMessage && s.hasError)}
+//       disabled={disabled}
+//       errorMessage={errorMessage}
+//       icon={<CalendarOutline className={clsx(s.calendarIcon, errorMessage && s.hasError)} />}
+//       label={label}
+//       {...rest}
+//     />
+//   )
+// }
+
+type CustomInputProps = {
+  disabled?: boolean
+  label?: ReactNode
+  required?: boolean
+  errorMessage?: string
+}
+
+const CustomInput = forwardRef<HTMLInputElement, CustomInputProps>(
+  ({ disabled, label, required, errorMessage, ...rest }, ref) => {
+    const classNames = {
+      icon: clsx(s.icon, disabled && s.disabled),
+      inputContainer: s.inputContainer,
+    }
+
+    return (
+        <div className={classNames.inputContainer}>
+          <input disabled={disabled} ref={ref} {...rest} />
+          <div className={classNames.icon}>
+          <CalendarOutline className={clsx(s.calendarIcon, errorMessage && s.hasError)} />
+          </div>
+        </div>
+    )
+  }
+)
+
+const CustomHeader = ({ date, decreaseMonth, increaseMonth }: ReactDatePickerCustomHeaderProps) => {
+  return (
+    <div className={s.headerContainer}>
+      <div className={s.monthsYear}>
+        <Typography variant={'bold_text16'}>{format(date, 'LLLL y')}</Typography>{' '}
+      </div>
+      <button className={s.button} onClick={decreaseMonth} type={'button'}>
+        {<ChevronLeft />}
+      </button>
+      <button className={s.button} onClick={increaseMonth} type={'button'}>
+        {<ChevronRight />}
+      </button>
     </div>
   )
 }
